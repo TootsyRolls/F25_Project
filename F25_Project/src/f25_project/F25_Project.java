0fc6b81989ff89e8d7d74393eb0eee585ec1bb5b
TootsyRolls <tonygian812@gmail.com>
2025-09-28 16:54:30 -0400

    Initial commit

---
+++ F25_Project/src/f25_project/F25_Project.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package f25_project;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author tonyg
+ */
+public class F25_Project extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+        stage.setTitle("8-Ball Simulator");
+    }
+    
+}
