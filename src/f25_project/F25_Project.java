1c8bbd34bbce1e176ba95a5c409a0a45b532dc0e
TurtlePlebu <NathanChg99@gmail.com>
2025-11-24 10:43:41 -0500

    FXMLs assigned
    All the fxml files necessary

--- src/f25_project/F25_Project.java
+++ src/f25_project/F25_Project.java
@@ -4,17 +4,44 @@
  */
 package f25_project;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author 2376190
  */
-public class F25_Project {
+public class F25_Project extends Application{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
-    
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Stage menu = new Stage();
+        Stage data = new Stage();
+        
+        FXMLLoader menuLoader = new FXMLLoader(getClass().getResource("view/Menu.fxml"));
+        FXMLLoader mainLoader = new FXMLLoader(getClass().getResource("view/MainTable.fxml"));
+        FXMLLoader dataLoader = new FXMLLoader(getClass().getResource("view/Data.fxml"));
+        
+        stage.setScene(new Scene(mainLoader.load()));
+        stage.setTitle("Playground");
+        stage.show();
+        
+        menu.setTitle("Menu");
+        menu.setScene(new Scene(menuLoader.load()));
+        menu.show();
+        
+        data.setTitle("Data Table");
+        data.setScene(new Scene(dataLoader.load()));
+        data.show();
+    }
 }
